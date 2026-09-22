9fd91a0805686bf4353e82bdf07d3eb88bd20e45
YHK0427 <bleach10905@gmail.com>
2026-09-22 11:06:24 +0900

    실습6 템플릿 렌더링

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -27,6 +27,12 @@ def age_any(num):
 @app.route("/age2/<int:num>")
 def age_int(num):
     return f"<h1>{num} 살 - 타입은 {type(num).__name__}</h1>"
+
+
+@app.route("/hi/<name>")
+def hi_template_render(name):
+    return render_template('hi.html', name = name)
+
 
 
 
